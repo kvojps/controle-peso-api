7e15b90a35711fa6b8c624f03ca2a7f8a684a676
kvojps <joseferreira.santosjunior@upe.br>
2022-05-11 10:49:00 -0300

    estrutura inicial do projeto

---
+++ src/main/java/br/upe/controlepesoapi/ControlePesoApiApplication.java
@@ -0,0 +1,13 @@
+package br.upe.controlepesoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePesoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePesoApiApplication.class, args);
+	}
+
+}
