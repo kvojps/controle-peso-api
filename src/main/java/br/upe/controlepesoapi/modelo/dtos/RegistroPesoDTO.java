2f2d10d3aad8c293d0265b18e923ee58b014dda4
kvojps <joseferreira.santosjunior@upe.br>
2022-06-20 15:14:58 -0300

    corrigindo bug RegistroPesoDTO

--- src/main/java/br/upe/controlepesoapi/modelo/dtos/RegistroPesoDTO.java
+++ src/main/java/br/upe/controlepesoapi/modelo/dtos/RegistroPesoDTO.java
@@ -1,28 +1,22 @@
 package br.upe.controlepesoapi.modelo.dtos;
 
-import java.time.LocalDateTime;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-import br.upe.controlepesoapi.modelo.entidades.RegistroPeso;
-import br.upe.controlepesoapi.modelo.entidades.Usuario;
+import org.joda.time.LocalDate;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
 public class RegistroPesoDTO {
-	private Long id;
+  private Long id;
 
-	@NotEmpty(message = "Informe o peso")
-	private int peso;
+  @NotEmpty(message = "Informe o peso")
+  private int peso;
 
-	@NotEmpty(message = "Informe a data")
-	private LocalDateTime data;
+  @NotEmpty(message = "Informe a data")
+  private LocalDate data;
 
-	@NotNull(message = "Informe o identificador do usuário")
-	private Long idUsuario;
+  @NotNull(message = "Informe o identificador do usuário")
+  private Long idUsuario;
 }
